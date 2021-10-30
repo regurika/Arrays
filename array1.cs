0e09c75604032c30887df6d871e9a6533b6af528
regurika <86030308+regurika@users.noreply.github.com>
2021-10-30 14:12:27 +0800

    Add files via upload

---
+++ array1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Array1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        int[] arr = new int[10]; 
+        int i;  
+
+        Console.Write("Input 10 elements in the array :\n");  
+
+    for(i=0; i<10; i++)  
+    {  
+	    Console.Write("element - {0} : ",i);
+	    arr[i] = Convert.ToInt32(Console.ReadLine());  		
+    }  
+  
+        Console.Write("\nElements in array are: ");  
+    for(i=0; i<10; i++)  
+    {  
+        Console.Write("{0}  ", arr[i]);  
+    } 
+        Console.Write("\n");	
+  }
+}
+}
+
+
